8435e3710907cbedc58dfcad0861416567eccc9c
denis.tikhonov <ozy@mailserver.ru>
2015-07-21 00:32:10 +0300

    while link

--- commands/RivegaucheLinkController.php
+++ commands/RivegaucheLinkController.php
@@ -25,41 +25,54 @@ class RivegaucheLinkController extends Controller
      */
     public function actionIndex()
     {
-        /** @var $entity RivegaucheCategory*/
+        /** @var $entity RivegaucheCategory */
         $entity = new RivegaucheCategory();
+        $offset = 0; // начало отсчета
 
-        foreach ($entity->getLinks() as $link) {
-            $page = 0;
-            $client = new Client();
-            $i = 1;
-            do {
-                $crawler = $client->request('GET', $link['link'].'&page='.$page);
+        do {
+            $links = $entity->getLinks($offset, 5);
+            if (!empty($links)) {
+                foreach ($links as $link) {
+                    $page = 0;
+                    $client = new Client();
+                    do {
+                        $crawler = $client->request('GET', $link['link'].'&page='.$page);
 
-                $urls = $crawler->filter('div.es_product_images a')->each(function ($node) {
-                    $href = $node->attr('href');
-                    $url = $this->url.$href;
+                        $urls = $crawler->filter('div.es_product_images a')->each(function ($node) {
+                            $href = $node->attr('href');
+                            $url = $this->url.$href;
 
-                    return $url;
-                });
+                            return $url;
+                        });
 
-                if(empty($urls)) {$i = 0;}
+                        $i = (empty($urls)) ? 0 : 1; // выход из цикла
 
-                foreach ($urls as $key => $url) {
-                    $linkModel = RivegaucheLink::findOne(['link' => $url]);
-                    if (!$linkModel) {
-                        $linkModel = new RivegaucheLink();
-                    }
-                    $linkModel->link = $url;
+                        foreach ($urls as $key => $url) {
+                            $linkModel = RivegaucheLink::findOne(['link' => $url]);
+                            if (!$linkModel) {
+                                $linkModel = new RivegaucheLink();
+                            }
+                            $linkModel->link = $url;
 
-                    $linkModel->group = $link['group'];
-                    $linkModel->category = $link['category'];
-                    $linkModel->sub_category = $link['sub_category'];
-                    $linkModel->save();
+                            $linkModel->group = $link['group'];
+                            $linkModel->category = $link['category'];
+                            $linkModel->sub_category = $link['sub_category'];
+                            $linkModel->save();
+                        }
+
+                        $page++;
+                    } while ($i > 0);
+
                 }
-
-                $page++;
-            } while($i > 0);
-        }
+                $z = 1;
+                $offset += 5;
+                unset($links);
+                unset($links);
+                unset($client);
+            } else {
+                $z = 0;
+            }
+        } while ($z > 0);
 
         return 0;
     }
