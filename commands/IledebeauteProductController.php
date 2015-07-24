77c42f88d63e43cc85087d3032eb9976266ec2e1
denis.tikhonov <ozy@mailserver.ru>
2015-07-24 22:20:53 +0300

    ildebotte get product

--- commands/IledebeauteProductController.php
+++ commands/IledebeauteProductController.php
@@ -3,6 +3,8 @@
 namespace app\commands;
 
 use app\models\IledebeauteLink;
+use app\models\IledebeautePrice;
+use app\models\IledebeauteProduct;
 use app\models\LetualLink;
 use app\models\LetualPrice;
 use app\models\LetualProduct;
@@ -35,36 +37,230 @@ class IledebeauteProductController extends Controller
 
             if (!empty($links)) {
                 foreach ($links as $link) {
+
                     $client = new Client();
                     $crawler = $client->request('GET', $link['link']);
 
-                    //print_r($crawler);die;
-                    $urls = $crawler->filter('div.productItemDescription h3.title a')->each(function ($node) {
-                        $href = $node->attr('href');
-                        $url = $this->url . substr($href, 0, strpos($href, ";"));
+                    //TODO Обработка
+                    $head = $crawler->filter('div.b-product__bigColumn p.title')->each(function ($node) {
+                        $brand = $node->filter('a.title__brand')->each(function ($subNode) {
+                            return $subNode->text();
+                        });
 
-                        return $url;
+                        $title = $node->text();
+                        $title = str_replace('\r', '', $title);
+                        $title = str_replace('\n', '', $title);
+                        $title = str_replace($brand, '', $title);
+                        $title = trim($title);
+                        $title = str_replace(' ', '', $title);
+
+                        return [
+                            'brand' => reset($brand),
+                            'title' => $title,
+                        ];
                     });
 
-                    foreach ($urls as $key => $url) {
-                        $linkModel = LetualLink::findOne(['link' => $url]);
-                        if (!$linkModel) {
-                            $linkModel = new LetualLink();
-                        }
-                        $linkModel->link = $url;
+                    if(empty(reset($head))) {
+                        $head = $crawler->filter('div.b-product__item__promo__content')->each(function ($node) {
+                            $title = $node->filter('div.b-product__item__promo__title h1')->each(function ($subNode) {
+                                return $subNode->text();
+                            });
 
-                        $linkModel->group = $link['group'];
-                        $linkModel->category = $link['category'];
-                        $linkModel->sub_category = $link['sub_category'];
-                        $linkModel->save();
+                            $brand = $node->filter('div.b-product__item__promo__logo a')->each(function ($subNode) {
+                                return $subNode->attr('href');
+                            });
+
+                            $title = reset($title);
+                            $title = str_replace('\r', '', $title);
+                            $title = str_replace('\n', '', $title);
+                            $title = trim($title);
+                            $title = str_replace(' ', '', $title);
+                            $title = str_replace('</span>', '', $title);
+                            $title = str_replace('<span>', '', $title);
+
+                            return [
+                                'brand' => reset($brand),
+                                'title' => $title,
+                            ];
+                        });
                     }
+
+                    $body = $crawler->filter('table.b-showcase')->each(function ($node) {
+                        $items = $node->filter('tr.b-showcase__item')->each(function ($sub) {
+                            $price = $sub->filter('td.b-showcase__item__price')->each(function ($sub) {
+                                $oldPrice = $sub->filter('span.old')->each(function ($sub) {
+                                    return $sub->text();
+                                });
+                                $newPrice = $sub->filter('span.new')->each(function ($sub) {
+                                    return $sub->text();
+                                });
+
+                                if(empty(reset($oldPrice)) && empty(reset($newPrice))) {
+                                    $newPrice = $sub->filter('span.def')->each(function ($sub) {
+                                        return $sub->text();
+                                    });
+                                }
+
+                                $newPrice = trim(reset($newPrice));
+                                $newPrice = str_replace(' ', '', $newPrice);
+                                $newPrice = str_replace('*', '', $newPrice);
+                                $newPrice = str_replace('\r', '', $newPrice);
+                                $newPrice = str_replace('\n', '', $newPrice);
+
+                                $oldPrice = trim(reset($oldPrice));
+                                $oldPrice = str_replace(' ', '', $oldPrice);
+                                return [
+                                    'oldPrice' => (empty($oldPrice)) ? $newPrice : $oldPrice,
+                                    'newPrice' => $newPrice,
+                                ];
+                            });
+
+                            $article = $sub->filter('td.b-showcase__item__descr p.b-showcase__item__brand')->each(function ($sub) {
+                                return $sub->text();
+                            });
+                            $image = $sub->filter('td.b-showcase__item__img img')->each(function ($sub) {
+                                return 'http::'.$sub->attr('src');
+                            });
+                            $description = $sub->filter('td.b-showcase__item__descr p.b-showcase__item__link')->each(function ($sub) {
+                                return $sub->text();
+                            });
+
+                            if(!empty(reset($description))) {
+                                $description = trim(reset($description));
+                                $description = str_replace(' ', '', $description);
+                                $description = str_replace('*', '', $description);
+                                $description = str_replace('\r', '', $description);
+                                $description = str_replace('\n', '', $description);
+                                $description = trim($description);
+                            }
+
+                            if(empty(reset($article)) && empty(reset($price))) {
+                                return [];
+                            }
+
+                            return [
+                                'price' => reset($price),
+                                'article' => reset($article),
+                                'image' => reset($image),
+                                'description' => $description,
+                            ];
+                        });
+                        return $items;
+                    });
+
+                    if(empty(reset($body))) {
+                        $body = $crawler->filter('tr.b-cart__showcase__item')->each(function ($sub) {
+                            $price = $sub->filter('td.b-cart__showcase__item__price')->each(function ($sub) {
+                                $oldPrice = $sub->filter('span.old')->each(function ($sub) {
+                                    return $sub->text();
+                                });
+
+                                $newPrice = $sub->filter('span.new')->each(function ($sub) {
+                                    return $sub->text();
+                                });
+
+                                if (empty(reset($oldPrice)) && empty(reset($newPrice))) {
+                                    $newPrice = $sub->filter('span.def')->each(function ($sub) {
+                                        return $sub->text();
+                                    });
+                                }
+
+                                $newPrice = trim(reset($newPrice));
+                                $newPrice = str_replace(' ', '', $newPrice);
+                                $newPrice = str_replace('*', '', $newPrice);
+                                $newPrice = str_replace('\r', '', $newPrice);
+                                $newPrice = str_replace('\n', '', $newPrice);
+
+                                $oldPrice = trim(reset($oldPrice));
+                                $oldPrice = str_replace(' ', '', $oldPrice);
+
+                                return [
+                                    'oldPrice' => (empty($oldPrice)) ? $newPrice : $oldPrice,
+                                    'newPrice' => $newPrice,
+                                ];
+                            });
+
+                            $article = $sub->filter('td p.b-cart__showcase__item__brand')->each(function (
+                                $sub
+                            ) {
+                                return $sub->text();
+                            });
+                            $image = $sub->filter('td.b-cart__showcase__item__img img')->each(function ($sub) {
+                                return $this->url.$sub->attr('src');
+                            });
+                            $description = $sub->filter('td.b-showcase__item__descr p.b-showcase__item__link')->each(function (
+                                $sub
+                            ) {
+                                return $sub->text();
+                            });
+                            $description = trim(reset($description));
+                            $description = str_replace(' ', '', $description);
+                            $description = str_replace('*', '', $description);
+                            $description = str_replace('\r', '', $description);
+                            $description = str_replace('\n', '', $description);
+                            $description = trim($description);
+
+                            return [
+                                'price' => reset($price),
+                                'article' => reset($article),
+                                'image' => reset($image),
+                                'description' => $description,
+                            ];
+                        });
+                    }
+
+                    $result = [
+                        'head' => reset($head),
+                        'body' => reset($body),
+                    ];
+
+                    if(!empty($result)) {
+                        foreach ($result['body'] as $item) {
+                            if(!empty($item['article'])) {
+                                $product = IledebeauteProduct::findOne(['article' => $item['article']]);
+                                if (!$product) {
+                                    $product = new IledebeauteProduct();
+                                }
+
+                                $product->brand = $result['head']['brand'];
+                                $product->title = $result['head']['title'];
+                                $product->article = $item['article'];
+                                $product->category = $link['category'];
+                                $product->group = $link['group'];
+                                $product->link = $link['link'];
+                                $product->sub_category = $link['sub_category'];
+                                $product->image_link = $item['image'];
+                                $product->description = $item['description'];
+
+                                $iledebeautePrice = new IledebeautePrice();
+                                $iledebeautePrice->article = $item['article'];
+
+                                if (!empty($item['price']['newPrice'])) {
+                                    $iledebeautePrice->new_price = $item['price']['newPrice'];
+                                }
+                                if (!empty($item['price']['oldPrice'])) {
+                                    $iledebeautePrice->old_price = $item['price']['oldPrice'];
+                                }
+                                $product->save();
+                                if (!empty($iledebeautePrice)) {
+                                    $iledebeautePrice->save();
+                                }
+                            }
+                        }
+                    }
+
+                    unset($node);
+                    unset($subNode);
+                    unset($head);
+
+
+                   // $result[] = reset($body);
                 }
 
+
                 $z = 1;
-                $offset += 5;
+                $offset += 20;
                 unset($links);
-                unset($linkModel);
-                unset($urls);
                 unset($client);
 
             } else {
--- models/IledebeauteProduct.php
+++ models/IledebeauteProduct.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "iledebeaute_product".
@@ -86,5 +88,22 @@ class IledebeauteProduct extends \yii\db\ActiveRecord
     public function getIledebeautePrices()
     {
         return $this->hasMany(IledebeautePrice::className(), ['article' => 'article']);
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehavior',
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 }
