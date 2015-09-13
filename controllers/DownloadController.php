d271f4fe52754aae667370c87ea0985892d1031f
denis.tikhonov <ozy@mailserver.ru>
2015-09-13 16:53:49 +0300

    fix rivegauche + xls

--- controllers/DownloadController.php
+++ controllers/DownloadController.php
@@ -2,7 +2,12 @@
 
 namespace app\controllers;
 
+use app\helpers\ExcelComponent;
+use app\models\IledebeauteProduct;
 use app\models\LetualProduct;
+use app\models\LetualProductSearch;
+use app\helpers\ExcelXML;
+use app\models\RivegaucheProduct;
 use Yii;
 use yii\filters\AccessControl;
 use yii\helpers\Url;
@@ -25,7 +30,7 @@ class DownloadController extends Controller
                     ],
                     [
                         'allow' => true,
-                        'actions' => ['download'],
+                        'actions' => ['download', 'export'],
                         'roles' => ['@'],
                     ],
                 ],
@@ -54,56 +59,43 @@ class DownloadController extends Controller
 
     public function actionDownload()
     {
-        $request = Yii::$app->getRequest()->post();
-        if (array_key_exists('letual',$request)) {
+        $request = Yii::$app->getRequest()->get();
+
+        if($request['company'] == 'letual') {
             $command = Yii::$app->getDb()->createCommand('SELECT * FROM letual_product');
+            $attr = new LetualProduct();
             $reader = $command->query();
-
-            $phpexcel = new \PHPExcel(); // Создаём объект PHPExcel
-            /* Каждый раз делаем активной 1-ю страницу и получаем её, потом записываем в неё данные */
-            $page = $phpexcel->setActiveSheetIndex(0); // Делаем активной первую страницу и получаем её
-            $page->setCellValue("A1", "id");
-            $page->setCellValue("B1", "article");
-            $page->setCellValue("C1", "link");
-            $page->setCellValue("D1", "group");
-            $page->setCellValue("E1", "category");
-            $page->setCellValue("F1", "sub_category");
-            $page->setCellValue("G1", "brand");
-            $page->setCellValue("H1", "title");
-            $page->setCellValue("I1", "description");
-            $page->setCellValue("J1", "old_price");
-            $page->setCellValue("K1", "new_price");
-            $page->setCellValue("L1", "image_link");
-            $page->setCellValue("M1", "updated_at");
-            $page->setTitle("Выгрузка Летуаль"); // Заголовок делаем "Example"
-            $cacheMethod = \PHPExcel_CachedObjectStorageFactory::cache_to_phpTemp;
-            $cacheSettings = array( 'memoryCacheSize ' => '312MB');
-            \PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
-            $i = 2;
-
-            while ($row = $reader->read()) {
-                $page->setCellValue("A".$i, $row["id"]);
-                $page->setCellValue("B".$i, $row["article"]);
-                $page->setCellValue("C".$i, $row["link"]);
-                $page->setCellValue("D".$i, $row["group"]);
-                $page->setCellValue("E".$i, $row["category"]);
-                $page->setCellValue("F".$i, $row["sub_category"]);
-                $page->setCellValue("G".$i, $row["brand"]);
-                $page->setCellValue("H".$i, $row["title"]);
-                $page->setCellValue("I".$i, $row["description"]);
-                $page->setCellValue("J".$i, $row["old_price"]);
-                $page->setCellValue("K".$i, $row["new_price"]);
-                $page->setCellValue("L".$i, $row["image_link"]);
-                $page->setCellValue("M".$i, $row["updated_at"]);
-                $i++;
-            }
-
-            /* Начинаем готовиться к записи информации в xlsx-файл */
-            $objWriter = \PHPExcel_IOFactory::createWriter($phpexcel, 'Excel2007');
-            /* Записываем в файл */
-            $file = sprintf('example_%s.xlsx',time());
-            $objWriter->save(sprintf('%s/web/files/%s', \Yii::$app->basePath, $file));
-            echo \yii\helpers\Html::a('<i class="fa fa-download"></i>&nbsp;Скачать','@web/files/'.$file, ['class' => 'btn btn-primary']);
+        } elseif($request['company'] == 'rive') {
+            $command = Yii::$app->getDb()->createCommand('SELECT * FROM rivegauche_product');
+            $attr = new RivegaucheProduct();
+            $reader = $command->query();
+        } elseif($request['company'] == 'ile') {
+            $command = Yii::$app->getDb()->createCommand('SELECT * FROM iledebeaute_product');
+            $attr = new IledebeauteProduct();
+            $reader = $command->query();
+        } else {
+            $let = [];
         }
+        //sprintf('%s/web/files/%s', \Yii::$app->basePath, sprintf('example_%s.xls',time()))
+        $xls = new ExcelXML();
+
+        $header_style = array(
+            'bold'       => 1,
+            'size'       => '12',
+            'color'      => '#FFFFFF',
+            'bgcolor'    => '#4F81BD'
+        );
+
+        $xls->add_style('header', $header_style);
+        //$xls->debug();
+        $xls->add_row($attr->attributes(), 'header');
+
+        while ($row = $reader->read()) {
+            $xls->add_row($row);
+        }
+
+        $xls->create_worksheet('Users');
+        $xml = $xls->generate();
+        $xls->download(sprintf('%s_%s.xls', $request['company'], time()));
     }
 }
