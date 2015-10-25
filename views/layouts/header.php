38c6cecdc365fee9729b32939f1896c0bd1bb7e6
denis.tikhonov <ozy@mailserver.ru>
2015-10-25 19:00:17 +0300

    Переход на parser service

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -23,42 +23,23 @@ use yii\bootstrap\NavBar;
                 <li class="dropdown notifications-menu">
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#" aria-expanded="false">
                         <i class="fa fa-bell-o"></i>
-                        <span class="label label-warning">10</span>
+                        <span class="label label-warning">0</span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li class="header">You have 10 notifications</li>
+                        <li class="header">Сообщения системы</li>
                         <li>
                             <!-- inner menu: contains the actual data -->
                             <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 200px;"><ul class="menu" style="overflow: hidden; width: 100%; height: 200px;">
                                     <li>
                                         <a href="#">
-                                            <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                                            page and may cause design problems
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-users text-red"></i> 5 new members joined
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-user text-light-blue"></i> You changed your username
+                                            <i class="fa fa-warning text-yellow"></i> Нужно заполнить пустые бренды(0)
                                         </a>
                                     </li>
                                 </ul><div class="slimScrollBar" style="background: rgb(0, 0, 0) none repeat scroll 0% 0%; width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 195.122px;"></div><div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51) none repeat scroll 0% 0%; opacity: 0.2; z-index: 90; right: 1px;"></div></div>
                         </li>
-                        <li class="footer"><a href="#">View all</a></li>
+                        <li class="footer">
+                            <a href="#">Данное меню в работе</a>
+                        </li>
                     </ul>
                 </li>
                 <li><a href="<?= \Yii::$app->getUrlManager()->createUrl("/upload/inform");  ?>">Загрузка информ.продукта</a></li>
