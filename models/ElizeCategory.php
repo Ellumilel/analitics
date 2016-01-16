294715d48b995cc21dfc27e5995c57f0a1cd782f
denis.tikhonov <ozy@mailserver.ru>
2016-01-16 12:34:03 +0300

    new elize category add

---
+++ models/ElizeCategory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "elize_category".
+ *
+ * @property integer $id
+ * @property string $link
+ * @property string $group
+ * @property string $category
+ * @property string $sub_category
+ */
+class ElizeCategory extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'elize_category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['link'], 'required'],
+            [['link'], 'string'],
+            [['group', 'category', 'sub_category'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'link' => 'Ссылка',
+            'group' => 'Группа',
+            'category' => 'Категория',
+            'sub_category' => 'Подкатегория',
+        ];
+    }
+
+    /**
+     * @param $offset
+     * @param $limit
+     *
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function getLinks($offset, $limit)
+    {
+        return $this::find()->offset($offset)->limit($limit)->all();
+    }
+}
---
+++ views/elize-category/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\ElizeCategory */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="elize-category-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'link')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'group')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'category')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'sub_category')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'создать' : 'обновить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
